1cb45e87492f633366c616fdfcddac9c3c0c2dad
Arpita <156744708+Arpitaum@users.noreply.github.com>
2024-01-21 16:26:14 +0530

    Update reducers.jsx

--- src/context/reducers.jsx
+++ src/context/reducers.jsx
@@ -2,27 +2,46 @@ export const tasksReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TASK":
       return {
-        ...state, tasks: [...state.tasks, {
-          id: Math.floor(Math.random() * 1000), text: action.payload, isCompleted: false
-        }]
+        ...state,
+        tasks: [
+          ...state.tasks,
+          {
+            id: Math.floor(Math.random() * 1000),
+            text: action.payload,
+            isCompleted: false,
+          },
+        ],
       };
-    
+
     case "DELETE_TASK":
       return {
-        ...state, tasks: state.tasks.filter(task => task.id !== action.payload.id)
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload.id),
       };
-    
+
     case "TOGGLE_TASK_STATUS":
       return {
-        ...state, tasks: state.tasks.map(task => {
-          if(task.id !== action.payload.id) {
+        ...state,
+        tasks: state.tasks.map((task) => {
+          if (task.id !== action.payload.id) {
             return task;
           }
-          return {...task, isCompleted: !task.isCompleted}
-        })
+          return { ...task, isCompleted: !task.isCompleted };
+        }),
+      };
+
+    case "EDIT_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.map((task) => {
+          if (task.id !== action.payload.id) {
+            return task;
+          }
+          return { ...task, text: action.payload.text };
+        }),
       };
 
     default:
       return state;
   }
-}
+};
